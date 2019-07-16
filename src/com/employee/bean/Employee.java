881b656bd5044189c38e4c4b39ad329b5599612e
Shounak Kulkarni <shokulka@publicisgroupe.net>
2019-07-16 08:45:57 +0530

    Assessment 2 push

---
+++ src/com/employee/bean/Employee.java
@@ -0,0 +1,16 @@
+package com.employee.bean;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+	private String id;
+	private String firstName;
+	private String lastName;
+	private int salary;
+	private String designation;
+	private String password;
+	
+	
+}
